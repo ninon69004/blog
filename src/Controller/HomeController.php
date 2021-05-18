96510979520b76259226805024dd361f9807deb4
Ninon Auburtin <ninon.auburtin@newgeneration-sr.com>
2021-05-18 19:38:44 +0200

    hobby styling

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\ProfessionalExperiences;
+use App\Entity\Hobbies;
+use App\Entity\Education;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,13 +24,35 @@ class HomeController extends AbstractController
      */
     public function exp(): Response
     {
-        return $this->render('home/exp.html.twig');
+        $repo = $this->getDoctrine()->getRepository(ProfessionalExperiences::class);
+
+        $exps = $repo->findAll();
+        return $this->render('home/exp.html.twig', [
+            'exps'=>$exps
+        ]);
     }
     /**
      * @Route("/hobby", name="hobby")
      */
     public function hobby(): Response
     {
-        return $this->render('home/hobby.html.twig');
+        $repo = $this->getDoctrine()->getRepository(Hobbies::class);
+
+        $hobbies = $repo->findAll();
+        return $this->render('home/hobby.html.twig', [
+            'hobbies'=> $hobbies
+        ]);
+    }
+    /**
+     * @Route("/education", name="education")
+     */
+    public function education(): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Education::class);
+
+        $educa = $repo->findAll();
+        return $this->render('home/education.html.twig', [
+            'educations' => $educa
+        ]);
     }
 }
