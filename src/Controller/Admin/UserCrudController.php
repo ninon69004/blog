15015c5bc806ac6175059a09de63753e3fb6b4fc
Ninon Auburtin <ninon.auburtin@newgeneration-sr.com>
2021-05-19 12:00:41 +0200

    password

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -24,7 +25,7 @@ class UserCrudController extends AbstractCrudController
             TextField::new('firstName'),
             TextField::new('lastName'),
             EmailField::new('email'),
-            TextField::new('password'),
+            TextField::new('password')->hideOnIndex()->setFormType(PasswordType::class),
             AssociationField::new('articles'),
         ];
     }
